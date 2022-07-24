b54ce15a7266b983b79f06a522a815e629ac4512
gamma-410 <mail@gamma410.win>
2022-07-24 12:00:07 +0900

    if文 -> switch文へ

--- main.go
+++ main.go
@@ -9,25 +9,35 @@ import (
 )
 
 func main() {
-  flag.Parse() // 引数を解析
+  flag.Parse() 
   str := flag.Arg(0) 
-
-  f, err := os.Open(str)
+  
+  f, err := os.Open("todo.txt")
 
   if err != nil {
-    fmt.Println("えらー")
+    fmt.Println("Error: Cannot find filename.")
   }
 
-  if str == "" {
-    fmt.Println("Error: cannot find filename.")
-  } else {
-    data := make([]byte, 1024)
-    count, err := f.Read(data)
+  switch str {
+    case "list":
+      data := make([]byte, 1024)
+      count, err := f.Read(data)
     
-    if err != nil {
-      fmt.Println("えらー")
-    }
-    fmt.Println(string(data[:count]))
+      if err != nil {
+        fmt.Println("Error: Failed to read the file.")
+      }
+      fmt.Println(string(data[:count]))
+
+    case "add":
+      return
+
+    case "delete":
+      return
+
+    default:
+      fmt.Println("Error: No arguments are specified.")
+      return
+
   }
 
 }
