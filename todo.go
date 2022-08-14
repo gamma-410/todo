9e8c7086f363a80f5fa12e688810069c7a9eb8c2
gamma-410 <mail@gamma410.win>
2022-08-14 09:03:33 +0900

    fmt

--- todo.go
+++ todo.go
@@ -1,74 +1,74 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-  flag.Parse()
-  arg := flag.Arg(0)
-  switch arg {
-    case "init":
-      initTodo()
-    case "add":
-      arg1 := flag.Arg(1)
-      add(arg1)    
-    case "list":
-      list()
-    case "delete":
-    
-    default:
-      fmt.Println("error: received command was not found.")
-  }
+	flag.Parse()
+	arg := flag.Arg(0)
+	switch arg {
+	case "init":
+		initTodo()
+	case "add":
+		arg1 := flag.Arg(1)
+		add(arg1)
+	case "list":
+		list()
+	case "delete":
+
+	default:
+		fmt.Println("error: received command was not found.")
+	}
 }
 
 func initTodo() {
-  file, err := os.Create(".todo") 
-  // 学び: "" で囲むとstring, '' で囲むとruneになる
+	file, err := os.Create(".todo")
+	// 学び: "" で囲むとstring, '' で囲むとruneになる
 
-  if err != nil {
-    panic(err)
-  }
-  file.Close()
-  fmt.Println("success: successfully initialized todo.")
+	if err != nil {
+		panic(err)
+	}
+	file.Close()
+	fmt.Println("success: successfully initialized todo.")
 }
 
-func add(arg string) { 
-  file, err := os.OpenFile(".todo", os.O_WRONLY|os.O_APPEND, 0666)
-  // 引数: ファイルのパス, フラグ, パーミッション
-  
-  if err != nil {
-    panic(err)
-  }
+func add(arg string) {
+	file, err := os.OpenFile(".todo", os.O_WRONLY|os.O_APPEND, 0666)
+	// 引数: ファイルのパス, フラグ, パーミッション
 
-  defer file.Close() 
-  // 漢文で言うレ点? 下の処理が終わったら実行だと思う
-  
-  fmt.Fprintln(file, arg) 
-  // "F" がついてるものは書き込み先を明示的に指定できる fmt.Fprintln(パス, 内容)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	// 漢文で言うレ点? 下の処理が終わったら実行だと思う
+
+	fmt.Fprintln(file, arg)
+	// "F" がついてるものは書き込み先を明示的に指定できる fmt.Fprintln(パス, 内容)
 }
 
 func list() {
-  file, err := os.Open(".todo")
-  
-  if err != nil {
-    panic(err)
-  }
+	file, err := os.Open(".todo")
 
-  data := make([]byte, 1024)
-  // UTF-8 - English: 1byte, Japanese: 3byte
-  // data - ファイルに入っている文字列を格納するスライス
+	if err != nil {
+		panic(err)
+	}
 
-  count, err := file.Read(data)
-  // f.Read(data) - おそらくこの時に読み込んだデータをdataスライスに渡してる...?
-  // count - Read()をした結果、何byte格納されているかがint型で入っている
+	data := make([]byte, 1024)
+	// UTF-8 - English: 1byte, Japanese: 3byte
+	// data - ファイルに入っている文字列を格納するスライス
 
-  if err != nil {
-    panic(err)
-  }
+	count, err := file.Read(data)
+	// f.Read(data) - おそらくこの時に読み込んだデータをdataスライスに渡してる...?
+	// count - Read()をした結果、何byte格納されているかがint型で入っている
 
-  fmt.Println(string(data[:count]))
-  // この形は Slice[:end] - data の先頭から end -1 までを取得
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data[:count]))
+	// この形は Slice[:end] - data の先頭から end -1 までを取得
 }
